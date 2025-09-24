584d436d34c2dc41404d4f9685b2484dc2623e12
Godis Nanu <godisnanu@gmail.com>
2025-09-24 19:23:35 -0400

    staging and creating file structure

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="page">
+        <div className="page__content">
+          /*Header*/ /* Components of the Homepage */ /* Components of the
+          Profile Page */ /* Components of the Staff Page */ /* Modals */ /*
+          Footer */
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
