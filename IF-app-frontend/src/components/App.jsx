c2c4e109bcc68a5f9b14af85a1e74a57138e1cf1
Godis Nanu <godisnanu@gmail.com>
2025-09-28 18:41:19 -0400

    completed header markup and styling

--- IF-app-frontend/src/components/App.jsx
+++ IF-app-frontend/src/components/App.jsx
@@ -1,16 +1,66 @@
+import { useEffect, useState } from "react";
 import "../blocks/page.css";
 import { Route, Routes, useNavigate } from "react-router-dom";
+import Loading from "./Preloader";
+import Header from "./Header";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [activeModal, setActiveModal] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  function handleAddClassClick() {
+    setActiveModal("add-class");
+  }
+
+  function handleLogoutClick() {
+    setIsLoggedIn(false);
+    setActiveModal("logout");
+  }
+
+  function handleLoginClick() {
+    setIsLoggedIn(true);
+    setActiveModal("login");
+  }
+
+  function handleJoinClick() {
+    setActiveModal("join");
+  }
+
   return (
     <>
-      <div className="page">
-        <div className="page__content">
-          /*Header*/ /* Components of the Homepage */ /* Components of the
-          Profile Page */ /* Components of the Staff Page */ /* Modals */ /*
-          Footer */
-        </div>
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="page">
+            <div className="page__content">
+              <Header
+                isLoggedIn={isLoggedIn}
+                handleAddClassClick={handleAddClassClick}
+                handleLogoutClick={handleLogoutClick}
+                handleLoginClick={handleLoginClick}
+                handleJoinClick={handleJoinClick}
+              />
+              {/*Header*/
+              /* Components of the Homepage */
+              /* Components of the
+          Profile Page */
+              /* Components of the Staff Page */
+              /* Modals */
+              /*
+          Footer */}
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
---
+++ IF-app-frontend/src/components/Preloader.jsx
@@ -0,0 +1,11 @@
+import "../blocks/page.css";
+
+const Loading = () => {
+  return (
+    <>
+      <div className="page__preloader"> Loading... </div>
+    </>
+  );
+};
+
+export default Loading;
